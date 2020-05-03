6018425111e1b42f776c666348c30074d99e4d55
thenextfreud <thenextfreud@gmail.com>
2020-05-03 16:45:34 -0700

    dynamic images

--- src/components/SlideShow/index.jsx
+++ src/components/SlideShow/index.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import { Fade } from "react-slideshow-image";
 import "./styles.css";
 
-import slide1 from "../../images/slide1.jpeg";
-import slide2 from "../../images/slide2.jpeg";
-import slide3 from "../../images/slide3.jpeg";
-import slide4 from "../../images/slide4.jpeg";
-
 const fadeProperties = {
 	duration: 5000,
 	transitionDuration: 500,
@@ -15,31 +10,37 @@ const fadeProperties = {
 	pauseOnHover: true,
 };
 
-export default (props) => {
-	return (
-		<div className="slide-container">
-			<Fade {...fadeProperties}>
-				<div className="each-fade">
-					<div className="image-container">
-						<img src={slide1} alt="" />
-					</div>
-				</div>
-				<div className="each-fade">
-					<div className="image-container">
-						<img src={slide2} alt="" />
-					</div>
-				</div>
-				<div className="each-fade">
-					<div className="image-container">
-						<img src={slide3} alt="" />
-					</div>
-				</div>
-				<div className="each-fade">
-					<div className="image-container">
-						<img src={slide4} alt="" />
-					</div>
-				</div>
-			</Fade>
+const Slide = ({ image, alt = "" }) => (
+	<div className="each-fade">
+		<div className="image-container">
+			<img src={image} alt={alt} />
 		</div>
-	);
-};
+	</div>
+);
+class SlideShow extends React.Component {
+	importAll(r) {
+		return r.keys();
+	}
+
+	componentWillMount() {
+		this.slides = this.importAll(
+			require.context("../../images/Slides", false, /\.(png|jpe?g|svg)$/),
+		);
+		console.log(this.slides);
+	}
+	render() {
+		return (
+			<div className="slide-container">
+				<Fade {...fadeProperties}>
+					<div className="each-fade">
+						<div className="image-container">
+							<img src={this.slides[0]} alt={""} />
+						</div>
+					</div>{" "}
+				</Fade>
+			</div>
+		);
+	}
+}
+
+export default SlideShow;
