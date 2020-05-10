59ee9df6a478d78a4a9bfc9fb6604a07c302134e
thenextfreud <thenextfreud@gmail.com>
2020-05-10 12:26:01 -0700

    added arrow images for slideshow

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -15,9 +15,9 @@ export default (props) => {
 					struggling businesses as the coronavirus outbreak worsened.
 					On March 25, 2020, SSAI changed its bylaws to serve as the
 					umbrella nonprofit for this effort. Our goal is to support local business while providing
-						meals to hospital workers in the New York and New Jersey
-						area who have no choice but to work through the COVID-19
-						outbreak.`
+					meals to hospital workers in the New York and New Jersey
+					area who have no choice but to work through the COVID-19
+					outbreak.`
 			}
 		/>
 	);
