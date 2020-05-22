14fef115160cc09358c5f1154ba999a16604b741
thenextfreud <thenextfreud@gmail.com>
2020-05-22 14:03:18 -0700

    modified subtitle

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -8,7 +8,7 @@ export default (props) => {
   return (
     <div>
       <div className="hosp-banner">
-        <h3 className="subTopTitle">Please support SSAI in our effort to</h3>
+        <h3 className="sub-title">Please support SSAI in our effort to</h3>
         <h1 className="title">Fuel Hospital Workers</h1>
         <div className="banner-donate">
           <DonateButton />
