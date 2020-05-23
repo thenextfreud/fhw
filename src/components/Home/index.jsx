608975b62e953390c1409bd305eb39d009601d12
thenextfreud <thenextfreud@gmail.com>
2020-05-22 17:35:59 -0700

    updated sub-title class

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,15 +5,17 @@ import DonateButton from "../DonateButton";
 import Card from "../Card";
 
 export default (props) => {
-  return (
-    <div>
-      <div className="hosp-banner">
-        <h3 className="sub-title">Please support SSAI in our effort to</h3>
-        <h1 className="title">Fuel Hospital Workers</h1>
-        <div className="banner-donate">
-          <DonateButton />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="hosp-banner">
+				<h3 className="sub-title">
+					Please support SSAI in our effort to
+				</h3>
+				<h1 className="title">Fuel Hospital Workers</h1>
+				<div className="banner-donate">
+					<DonateButton />
+				</div>
+			</div>
+		</div>
+	);
 };
