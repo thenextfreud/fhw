f2887d46d31b11e0345777a028fbda0c5cc04473
LegioTitanicus <alexbernstein7170@gmail.com>
2020-05-27 20:07:39 -0400

    put correct className "page-container" on enclosing div and changed related CSS

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import Card from "../Card";
 
 export default (props) => {
-	return (
-		<>
-			<Card
-				title={"The Effort"}
-				text={`Join us in powering the community that has given to us
+  return (
+    <div className="page-container">
+      <>
+        <Card
+          title={"The Effort"}
+          text={`Join us in powering the community that has given to us
 						much more than we can ever repay. With each individual
 						effort combined, we can do so much.`}
-			/>
-			<Card
-				title={`Why?`}
-				text={`Healthcare providers need the public’s support.
+        />
+        <Card
+          title={`Why?`}
+          text={`Healthcare providers need the public’s support.
 						New York and New Jersey have nearly a third of the
 						country's known COVID-19 cases. Hundreds of thousands
 						have been infected and tens of thousands have lost their
@@ -20,10 +21,10 @@ export default (props) => {
 						cannot handle the caseload, especially as they get sick
 						themselves. With the outbreak expected to get worse,
 						they need our support. One option: Fuel Hospital Workers!`}
-			/>
-			<Card
-				title={"About Us"}
-				text={`
+        />
+        <Card
+          title={"About Us"}
+          text={`
 					Sight Saving Academy of India (SSAI), founded in 2004, has
 					historically served to fill gaps in healthcare access for
 					populations most in need of support. Established in the New
@@ -35,7 +36,8 @@ export default (props) => {
 					meals to hospital workers in the New York and New Jersey
 					area who have no choice but to work through the COVID-19
 					outbreak.`}
-			/>
-		</>
-	);
+        />
+      </>
+    </div>
+  );
 };
