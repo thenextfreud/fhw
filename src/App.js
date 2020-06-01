146f03d1c9ca22c3cf25cfad4b85a45a602d6935
LegioTitanicus <alexbernstein7170@gmail.com>
2020-06-01 17:32:40 -0400

    fixed plasma link

--- src/App.js
+++ src/App.js
@@ -8,26 +8,28 @@ import Impact from "./components/Impact";
 import About from "./components/About";
 import Layout from "./components/Layout";
 import Letter from "./components/Letter";
+import Plasma from "./components/Plasma";
 
 class App extends React.Component {
-	UNSAFE_componentWillMount() {
-		document.title = "Fuel Hospital Workers!";
-	}
+  UNSAFE_componentWillMount() {
+    document.title = "Fuel Hospital Workers!";
+  }
 
-	render() {
-		return (
-			<Router>
-				<link rel="icon" href="./images/logo.png" />
-				<Layout>
-					<title>Fuel Hosipital Workers</title>
-					<Route exact path={p.home} component={Home} />
-					<Route path={p.about} component={About} />
-					<Route path={p.impact} component={Impact} />
-					<Route path={p.letter} component={Letter} />
-				</Layout>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <link rel="icon" href="./images/logo.png" />
+        <Layout>
+          <title>Fuel Hosipital Workers</title>
+          <Route exact path={p.home} component={Home} />
+          <Route path={p.about} component={About} />
+          <Route path={p.impact} component={Impact} />
+          <Route path={p.letter} component={Letter} />
+          <Route path={p.plasma} component={Plasma} />
+        </Layout>
+      </Router>
+    );
+  }
 }
 
 export default App;
